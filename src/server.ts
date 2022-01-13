d8e66c83098f0b3f442da7353847a96c67bf8e46
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-01-13 00:35:55 -0300

    Starting CRUD, routes and db

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
-import express from "express";
+import express, { json } from "express";
+import { db } from "./database/db";
+import { router } from "./routes";
 
 const app = express();
-app.listen(3000, () => {
+app.use(router);
+app.use(json());
+
+app.listen(3000, async () => {
+  await db.sync();
   console.log(`App ${process.env.PROJECT_NAME} running at 3000`);
 });
